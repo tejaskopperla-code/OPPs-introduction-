a4f2ad9be07c0a2619d5e9afa4adbcbf4d946cd0
tejaskopperla-code <tejas.kopperla@gmail.com>
2026-03-03 23:39:19 +0530

    Add files via upload

---
+++ Computer_price.py
@@ -0,0 +1,16 @@
+class Computer:
+
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print("Selling Price :  {}".fomrat|self.__maxprice)
+
+    def setMaxPrice(self,price):
+        self.__maxprice = price
+
+c = Computer()
+c.sell()
+
+c.setMaxPrice = (1000)
+c.sell()
